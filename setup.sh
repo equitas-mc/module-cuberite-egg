c73d5321b44e5055a05761e6daf9214a24438e18
Tobias Wilken <tooangel@tooangel.de>
2020-04-02 20:13:34 +0200

    Initialize and set the permissions

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
-rm Cuberite.tar.gz
+rm -f Cuberite.tar.gz
 wget https://builds.cuberite.org/job/Cuberite%20Linux%20x64%20Master/lastSuccessfulBuild/artifact/Cuberite.tar.gz
+
 rm -rf Cuberite
 mkdir Cuberite
 tar -x -C Cuberite -f Cuberite.tar.gz
+
+rm Cuberite.tar.gz
+
+# The Ranks.sqlite is only create on server start. to be able to update
+# permissions a dump is loaded here
+cat Ranks.sql | sqlite3 Cuberite/Ranks.sqlite
