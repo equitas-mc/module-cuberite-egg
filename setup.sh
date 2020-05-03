562e5d2ad38b288229fb45abe1784bcd40de7fa4
Tobias Wilken <tooangel@tooangel.de>
2020-05-03 21:07:50 +0200

    Use custom cuberite build

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 rm -f Cuberite.tar.gz
-wget https://builds.cuberite.org/job/Cuberite%20Linux%20x64%20Master/lastSuccessfulBuild/artifact/Cuberite.tar.gz
+#wget https://builds.cuberite.org/job/Cuberite%20Linux%20x64%20Master/lastSuccessfulBuild/artifact/Cuberite.tar.gz
+wget https://www.equitas-mc.net/Cuberite.tar.gz
 
 rm -rf Cuberite
 mkdir Cuberite
